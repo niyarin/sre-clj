5c9889484e40aa60240d4f661e63dbcadf73a777
niyarin <inoanet@gmail.com>
2019-07-14 22:08:14 +0900

    Add needed files.

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to sre-clj
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/sre_clj/core_test.clj
@@ -0,0 +1,7 @@
+(ns sre-clj.core-test
+  (:require [clojure.test :refer :all]
+            [sre-clj.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
